a026d2b3a98d32e9a8b90110aa4aac28aae81833
Zhe Chen <chenzhe95123@tamu.edu>
2017-10-06 21:39:53 +0000

    feature/log-in-out finish!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,15 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email]) 
     if user && (user.password == params[:session][:password])
       log_in user
-      render :json => {:login_status => true,:status => 200, :message => "Successful log in!"}
+      render :json => {:is_login? => true,:status => 200, :message => "Successful log in!"}
     else
-      render :json => {:login_status => false, :status => 200, :message => "Invalid email/password combination!"}
+      render :json => {:is_login? => false, :status => 200, :message => "Invalid email/password combination!"}
     end
   end
   
   def destroy
       log_out
-      render :json => {:login_status => false, :status => 200, :message => "Just log out!"}
+      render :json => {:is_login? => false, :status => 200, :message => "Just log out!"}
   end
 
 end
