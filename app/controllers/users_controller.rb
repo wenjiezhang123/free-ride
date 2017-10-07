c949aacec99f15f4ba2d569c89b1ffa2dc38fc38
yanzeli <yanzeli@tamu.edu>
2017-10-06 23:53:50 -0500

    comment some code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,24 +28,24 @@ class UsersController < ApplicationController
   end
 
   def update
-    if !User.find_by(email: "#{params[:user][:email]}").nil?
-      if params[:id] != User.find_by(email: "#{params[:user][:email]}").id.to_s
-        @email_existed = true
-      end
-    else
-      @email_existed = false
-    end
-
-    @user = User.find(params[:id])
-
-    if @user.valid? && !@email_existed
-      @user.update_attributes(user_params)
-      render :json => { :updated? => true, :status => 200, :message => "Successful update!", :data => @user }
-    elsif @user.valid?
-      render :json => { :updated? => false, :status => 200, :message => "Email entered has been used!", :data => @user }
-    else
-      render :json => { :data => nil, :status => 200, :message => "Invalid user id!" }
-    end
+    # if !User.find_by(email: "#{params[:user][:email]}").nil?
+    #   if params[:id] != User.find_by(email: "#{params[:user][:email]}").id.to_s
+    #     @email_existed = true
+    #   end
+    # else
+    #   @email_existed = false
+    # end
+    #
+    # @user = User.find(params[:id])
+    #
+    # if @user.valid? && !@email_existed
+    #   @user.update_attributes(user_params)
+    #   render :json => { :updated? => true, :status => 200, :message => "Successful update!", :data => @user }
+    # elsif @user.valid?
+    #   render :json => { :updated? => false, :status => 200, :message => "Email entered has been used!", :data => @user }
+    # else
+    #   render :json => { :data => nil, :status => 200, :message => "Invalid user id!" }
+    # end
   end
 
   def destroy
@@ -56,13 +56,13 @@ class UsersController < ApplicationController
     @user = User.find_by(email: "#{params[:user][:email]}")
     #@nickname_existed = !User.find_by(nickname: "#{params[:user][:nickname]}").nil?
     if @user
-      render :json => {:data => @user, :status => 200, :email_already_exist => true } #:nickname_already_exist => @nickname_existed}
+      render :json => { :data => @user, :status => 200, :email_already_exist => true } #:nickname_already_exist => @nickname_existed}
     else
       @user = User.create(user_params)
       if @user.valid?
-        render :json => {:data => @user, :status => 200, :email_already_exist => false } #:nickname_already_exist => @nickname_existed }
+        render :json => { :data => @user, :status => 200, :email_already_exist => false } #:nickname_already_exist => @nickname_existed }
       else
-        render :json => {:data => nil, :status => 200}
+        render :json => { :data => nil, :status => 200 }
       end
     end
   end
