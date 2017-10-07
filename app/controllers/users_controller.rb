79f328e2fdf87d3af6eea42fd3cd2d9d1d4081a0
yanzeli <yanzeli@tamu.edu>
2017-10-06 22:56:16 -0500

    [bug] the email check is wrong

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    
+
   end
 
   def update
@@ -26,19 +26,18 @@ class UsersController < ApplicationController
 
   # register a new user
   def create
-    @email_existed = !User.find_by(email: "#{params[:user][:email]}").nil?
+    @user = User.find_by(email: "#{params[:user][:email]}")
     #@nickname_existed = !User.find_by(nickname: "#{params[:user][:nickname]}").nil?
-    @user = User.create(user_params)
-    
-    if @user.valid? && !@email_existed #&& !@nickname_existed
-      render :json => {:data => @user, :status => 200, :email_already_exist => @email_existed } #:nickname_already_exist => @nickname_existed}
-    elsif @user.valid?
-      @user.destroy
-      render :json => {:data => @user, :status => 200, :email_already_exist => @email_existed } #:nickname_already_exist => @nickname_existed }
+    if @user
+      render :json => {:data => @user, :status => 200, :email_already_exist => true } #:nickname_already_exist => @nickname_existed}
     else
-      @user.destroy
-      render :json => {:data => nil, :status => 200}
+      @user = User.create(user_params)
+      if @user.valid?
+        render :json => {:data => @user, :status => 200, :email_already_exist => false } #:nickname_already_exist => @nickname_existed }
+      else
+        render :json => {:data => nil, :status => 200}
+      end
     end
   end
-  
+
 end
